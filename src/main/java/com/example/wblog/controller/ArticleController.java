7610cee091d24f3b5c2e9ae1ca8958a3062eca4b
chidangao2013 <reg2980@163.com>
2021-09-22 14:25:56 +0800

    fix(控制层):Article改动

--- src/main/java/com/example/wblog/controller/ArticleController.java
+++ src/main/java/com/example/wblog/controller/ArticleController.java
@@ -1,24 +1,27 @@
 package com.example.wblog.controller;
 
-
+import org.apache.commons.io.IOUtils;
 import com.example.wblog.bean.Article;
+import com.example.wblog.bean.RespBean;
 import com.example.wblog.mapper.ArticleMapper;
 import com.example.wblog.service.ArticleService;
 import com.example.wblog.utils.Util;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @RestController
 @RequestMapping("/article")
 
 public class ArticleController {
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
 
     @Autowired
     ArticleService articleService;
@@ -32,4 +35,72 @@ public class ArticleController {
         map.put("articles",articles);
         return map;
     }
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public RespBean addNewArticle(Article article) {
+        int result = articleService.addNewArticle(article);
+        if (result == 1) {
+            return new RespBean("success", article.getId() + "");
+        } else {
+            return new RespBean("error", article.getState() == 0 ? "文章保存失败!" : "文章发表失败!");
+        }
+    }
+//    图片上传
+    @RequestMapping(value = "/uploadimg", method = RequestMethod.POST)
+    public RespBean uploadImg(HttpServletRequest req, MultipartFile image) {
+        StringBuffer url = new StringBuffer();
+        String filePath = "/blogimg/" + sdf.format(new Date());
+        String imgFolderPath = req.getServletContext().getRealPath(filePath);
+        File imgFolder = new File(imgFolderPath);
+        if (!imgFolder.exists()) {
+            imgFolder.mkdirs();
+        }
+        url.append(req.getScheme())
+                .append("://")
+                .append(req.getServerName())
+                .append(":")
+                .append(req.getServerPort())
+                .append(req.getContextPath())
+                .append(filePath);
+        String imgName = UUID.randomUUID() + "_" + image.getOriginalFilename().replaceAll(" ", "");
+        try {
+            IOUtils.write(image.getBytes(), new FileOutputStream(new File(imgFolder, imgName)));
+            url.append("/").append(imgName);
+            return new RespBean("success", url.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new RespBean("error", "上传失败!");
+    }
+    @RequestMapping(value = "/{aid}", method = RequestMethod.GET)
+    public Article getArticleById(@PathVariable Long aid) {
+        return articleService.getArticleById(aid);
+    }
+
+    @RequestMapping(value = "/dustbin", method = RequestMethod.PUT)
+    public RespBean updateArticleState(Long[] aids, Integer state) {
+        if (articleService.updateArticleState(aids, state) == aids.length) {
+            return new RespBean("success", "删除成功!");
+        }
+        return new RespBean("error", "删除失败!");
+    }
+
+    @RequestMapping(value = "/restore", method = RequestMethod.PUT)
+    public RespBean restoreArticle(Integer articleId) {
+        if (articleService.restoreArticle(articleId) == 1) {
+            return new RespBean("success", "还原成功!");
+        }
+        return new RespBean("error", "还原失败!");
+    }
+//    统计页面
+    @RequestMapping("/dataStatistics")
+    public Map<String,Object> dataStatistics() {
+        Map<String, Object> map = new HashMap<>();
+        List<String> categories = articleService.getCategories();
+        List<Integer> dataStatistics = articleService.getDataStatistics();
+        map.put("categories", categories);
+        map.put("ds", dataStatistics);
+        return map;
+    }
+
 }
+
