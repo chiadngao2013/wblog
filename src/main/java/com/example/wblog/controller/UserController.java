773aba13c375958f3ae44da7a14c8fb9c8eb2d45
chidangao2013 <reg2980@163.com>
2021-10-11 20:32:11 +0800

    fix:首页跳转

--- src/main/java/com/example/wblog/controller/UserController.java
+++ src/main/java/com/example/wblog/controller/UserController.java
@@ -7,11 +7,16 @@ import com.example.wblog.service.UserService;
 import com.example.wblog.utils.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -19,6 +24,12 @@ public class UserController {
 
     @Autowired
     UserService userService;
+
+    @GetMapping("/index")
+    public ModelAndView index(HttpSession session) {
+        Map<String, Object> model = new HashMap<>();
+        return new ModelAndView("index.html", model);
+    }
 
     @RequestMapping("/currentUserName")
     public String currentUserName() {
