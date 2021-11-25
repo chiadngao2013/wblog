065b376666c1379162562c288bbe6c12b03eaf68
chidangao2013 <reg2980@163.com>
2021-11-25 15:48:35 +0800

    fix:跳转bug

--- src/main/java/com/example/wblog/config/WebSecurityConfig.java
+++ src/main/java/com/example/wblog/config/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .antMatchers("/admin/**","/reg").hasRole("超级管理员")///admin/**的URL都需要有超级管理员角色，如果使用.hasAuthority()方法来配置，需要在参数中加上ROLE_,如下.hasAuthority("ROLE_超级管理员")
                 .antMatchers("/admin/**").hasRole("超级管理员")
                 .anyRequest().authenticated()//其他的路径都是登录后即可访问
-                .and().formLogin().loginPage("/index").successHandler(new AuthenticationSuccessHandler() {
+                .and().formLogin().loginPage("/index.html").successHandler(new AuthenticationSuccessHandler() {
             @Override
             public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
                 httpServletResponse.setContentType("application/json;charset=utf-8");
